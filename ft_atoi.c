832f025ee970f193104656642a411ad191350b0f
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-02 20:18:40 +0300

    Add ft_atoi function implementation

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,21 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 17:14:56 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/02 17:16:50 by dpowdere         ###   ########.fr       */
+/*   Created: 2020/11/02 20:03:49 by dpowdere          #+#    #+#             */
+/*   Updated: 2020/11/02 20:17:46 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_atoi(const char *nptr)
+{
+	char			*s;
+	int				sign;
+	unsigned int	n;
 
-#include <stddef.h>
-
-int		ft_atoi(const char *nptr);
-size_t	ft_strlen(const char *s);
-
-#endif
+	sign = 1;
+	n = 0;
+	s = (char *)nptr;
+	while (*s == ' ' || *s == '\f' || *s == '\n' || *s == '\r'
+			|| *s == '\t' || *s == '\v')
+		++s;
+	if (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			sign = -1;
+		++s;
+	}
+	while (*s >= '0' && *s <= '9')
+		n = n * 10 + (*s - '0');
+	return (sign * n);
+}
