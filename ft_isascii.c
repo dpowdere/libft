e750acaa53f5f3ea6912a6301ff47fca199eaabb
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-03 10:59:59 +0300

    Add isascii and isprint funcs implementations

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 17:14:56 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/02 21:31:19 by dpowdere         ###   ########.fr       */
+/*   Created: 2020/11/02 21:45:55 by dpowdere          #+#    #+#             */
+/*   Updated: 2020/11/02 21:47:11 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stddef.h>
-
-int		ft_atoi(const char *nptr);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-
-#endif
+inline int	ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
