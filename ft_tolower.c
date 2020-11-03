c229b6e6ddc139b79ce88b7e5515038fe84eaccd
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-03 11:00:41 +0300

    Add tolower & toupper funcs implementations

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,28 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/02 17:14:56 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/02 21:31:19 by dpowdere         ###   ########.fr       */
+/*   Created: 2020/11/03 10:57:36 by dpowdere          #+#    #+#             */
+/*   Updated: 2020/11/03 10:58:55 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stddef.h>
-
-int		ft_atoi(const char *nptr);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-
-#endif
+int	ft_tolower(int c)
+{
+	return ('A' <= c && c <= 'Z' ? c - 'A' + 'a' : c);
+}
