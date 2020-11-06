5329a4522ceb99bce3ab7764a0450b1538999c4e
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-06 12:48:16 +0300

    Implement `strlcpy` without use of `strlen`

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,26 +6,27 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 13:19:47 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/04 13:41:17 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/06 12:40:49 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
+	i = 0;
 	if (size > 0)
 	{
-		i = 0;
 		while (i < size - 1 && src[i])
 		{
 			dst[i] = src[i];
 			++i;
 		}
 		dst[i] = '\0';
+		while (src[i])
+			++i;
 	}
-	return (ft_strlen(src));
+	return (i > 0 ? i - 1 : i);
 }
