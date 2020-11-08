6748ae736f859b92f2cda972641db8e0231c4fa5
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-08 15:18:33 +0300

    Implement `lstnew` bonus function

--- libft_bonus.h
+++ libft_bonus.h
@@ -6,17 +6,21 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 17:10:48 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/07 17:13:44 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/08 15:09:17 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_BONUS_H
 # define LIBFT_BONUS_H
 
-typedef struct		s_list
+# include <stdlib.h>
+
+typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
-}					t_list;
+}				t_list;
+
+t_list			*ft_lstnew(void *content);
 
 #endif
