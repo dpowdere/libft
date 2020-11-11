bf63b1b6d57cad396b264fff2268b613d2dc0f94
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-12 02:03:30 +0300

    Cast int to unsigned char so that `strncmp` returns a valid result

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 09:04:55 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/04 09:12:11 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/12 02:01:21 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+	while (i < n
+			&& ((unsigned char *)s1)[i] != '\0'
+			&& ((unsigned char *)s2)[i] != '\0'
+			&& ((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
 		++i;
-	return (s1[i] - s2[i]);
+	if (i == n)
+		--i;
+	return ((int)((unsigned char *)s1)[i] - (((unsigned char *)s2)[i]));
 }
