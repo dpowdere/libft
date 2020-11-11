c4a96dd4e4d2bcad72c1a6c4b0f8faac8aac528b
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-12 00:30:37 +0300

    `memcmp` does not have to count for null char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,15 +14,14 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*m1;
-	unsigned char	*m2;
+	size_t i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	m1 = (unsigned char *)s1;
-	m2 = (unsigned char *)s2;
-	while (i < n && m1[i] && m2[i] && m1[i] == m2[i])
+	while (i < n && ((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
 		++i;
-	--i;
-	return ((int)(m1[i] - m2[i]));
+	if (i == n)
+		--i;
+	return ((int)(((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]));
 }
