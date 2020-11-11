e7ca77d55dfe3821988f2c6153f69e6ef86e2b76
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-12 02:29:08 +0300

    Fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -19,14 +19,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	i = 0;
 	if (size > 0)
 	{
-		while (i < size - 1 && src[i])
+		while (i < size - 1 && src[i] && dst[i])
 		{
 			dst[i] = src[i];
 			++i;
 		}
 		dst[i] = '\0';
-		while (src[i])
-			++i;
 	}
-	return (i > 0 ? i - 1 : i);
+	while (src[i])
+		++i;
+	return (i);
 }
