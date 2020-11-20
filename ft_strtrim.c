9f90566d7a1431e535c23913b6e9dfed717e431c
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-21 00:03:30 +0300

    Protect `strtrim` against NULL pointers

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 11:49:51 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/07 12:38:14 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/20 23:55:15 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char *end;
 	char *trimmed;
 
+	if (!s1)
+		return (NULL);
 	start = (char *)s1;
-	while (*start && ft_strchr(set, *start))
+	while (*start && set && ft_strchr(set, *start))
 		++start;
 	end = start;
 	while (*end && *(end + 1))
 		++end;
-	while (end > start && ft_strchr(set, *end))
+	while (end > start && set && ft_strchr(set, *end))
 		--end;
 	trimmed = (char *)malloc((end - start + 2) * sizeof(char));
 	if (!trimmed)
