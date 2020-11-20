1d97868b15b93bf2da18a3e7f49c16dd523da7fa
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-20 23:37:33 +0300

    Make `strjoin` tolerate NULL pointer arguments

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 22:32:15 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/06 23:56:47 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/20 23:35:05 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len2;
 	size_t	i;
 
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
+	len1 = (s1 ? ft_strlen(s1) : 0);
+	len2 = (s2 ? ft_strlen(s2) : 0);
 	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (!str)
 	{
