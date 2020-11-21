f8733f42da6bf99fcdcab258db8d9e1cb5916141
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-21 18:52:35 +0300

    Fix wrong chars count in `split`

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 12:41:28 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/07 13:16:51 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/21 18:51:05 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,52 @@
 
 static void	ft___count(char const *s, char c, size_t *n_words, size_t *n_chars)
 {
-	char	*cursor;
 	int		state;
 
 	*n_words = (size_t)0;
 	*n_chars = (size_t)0;
-	cursor = (char *)s;
 	state = OUT_WORD;
-	while (*cursor)
+	while (*s)
 	{
-		if (*cursor == c && state == IN_WORD)
+		if (*s == c && state == IN_WORD)
 			state = OUT_WORD;
-		else if (*cursor != c && state == IN_WORD)
-			++*n_chars;
-		else if (*cursor != c && state == OUT_WORD)
+		else if (*s != c)
 		{
-			state = IN_WORD;
-			++*n_words;
+			++*n_chars;
+			if (state == OUT_WORD)
+			{
+				state = IN_WORD;
+				++*n_words;
+			}
 		}
-		++cursor;
+		++s;
 	}
 }
 
 static void	ft___fill(char const *s, char c, char **mem, size_t n_words)
 {
-	char	*read_cursor;
 	char	*write_cursor;
 	int		state;
 
 	*(mem + n_words) = NULL;
 	write_cursor = (char *)(mem + n_words + 1);
-	read_cursor = (char *)s;
 	state = OUT_WORD;
-	while (s && *read_cursor)
+	while (s && *s)
 	{
-		if (*read_cursor == c && state == IN_WORD)
+		if (*s == c && state == IN_WORD)
 		{
 			state = OUT_WORD;
 			*write_cursor++ = '\0';
 		}
-		else if (*read_cursor != c && state == IN_WORD)
-			*write_cursor++ = *read_cursor;
-		else if (*read_cursor != c && state == OUT_WORD)
+		else if (*s != c && state == IN_WORD)
+			*write_cursor++ = *s;
+		else if (*s != c && state == OUT_WORD)
 		{
 			state = IN_WORD;
-			*write_cursor = *read_cursor;
+			*write_cursor = *s;
 			*mem++ = write_cursor++;
 		}
-		++read_cursor;
+		++s;
 	}
 }
 
