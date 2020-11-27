d1e565305a578d8aa3462fda95a1828c95793eb4
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-27 12:31:33 +0300

    Make names of the variables and constants more articulate

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 12:41:28 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/25 12:48:31 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/27 12:22:58 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,27 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-#define IN_SEGMENT	1
-#define OUT_SEGMENT	0
+#define IN_SEGMENT		1
+#define IN_SEPARATOR	0
 
 static void	ft___count(char const *s, char c,
 					size_t *n_segments, size_t *n_chars)
 {
-	int		state;
+	int		where;
 
 	*n_segments = 0;
 	*n_chars = 0;
-	state = OUT_SEGMENT;
+	where = IN_SEPARATOR;
 	while (*s)
 	{
-		if (*s == c && state == IN_SEGMENT)
-			state = OUT_SEGMENT;
+		if (*s == c && where == IN_SEGMENT)
+			where = IN_SEPARATOR;
 		else if (*s != c)
 		{
 			++*n_chars;
-			if (state == OUT_SEGMENT)
+			if (where == IN_SEPARATOR)
 			{
-				state = IN_SEGMENT;
+				where = IN_SEGMENT;
 				++*n_segments;
 			}
 		}
@@ -51,23 +51,23 @@ static void	ft___count(char const *s, char c,
 static void	ft___fill(char const *s, char c, char **mem, size_t n_segments)
 {
 	char	*write_cursor;
-	int		state;
+	int		where;
 
 	*(mem + n_segments) = NULL;
 	write_cursor = (char *)(mem + n_segments + 1);
-	state = OUT_SEGMENT;
+	where = IN_SEPARATOR;
 	while (s && *s)
 	{
-		if (*s == c && state == IN_SEGMENT)
+		if (*s == c && where == IN_SEGMENT)
 		{
-			state = OUT_SEGMENT;
+			where = IN_SEPARATOR;
 			*write_cursor++ = '\0';
 		}
-		else if (*s != c && state == IN_SEGMENT)
+		else if (*s != c && where == IN_SEGMENT)
 			*write_cursor++ = *s;
-		else if (*s != c && state == OUT_SEGMENT)
+		else if (*s != c && where == IN_SEPARATOR)
 		{
-			state = IN_SEGMENT;
+			where = IN_SEGMENT;
 			*write_cursor = *s;
 			*mem++ = write_cursor++;
 		}
