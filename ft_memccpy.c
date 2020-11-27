ca4e6f225242f5d9be7cb2f1a9cd70cb5cd2d19e
Dorine Powdered <dpowdere@student.21-school.ru>
2020-11-27 18:51:13 +0300

    `memccpy` must copy no more than N chars, including a char argument

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 22:00:15 by dpowdere          #+#    #+#             */
-/*   Updated: 2020/11/04 22:15:48 by dpowdere         ###   ########.fr       */
+/*   Updated: 2020/11/27 18:38:54 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,12 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	destination = (unsigned char *)dst;
 	source = (unsigned char *)src;
 	i = 0;
-	while (i < n && source[i] != (unsigned char)c)
+	while (i < n)
 	{
 		destination[i] = source[i];
+		if (source[i] == (unsigned char)c)
+			return (destination + i + 1);
 		++i;
-	}
-	if (source[i] == (unsigned char)c)
-	{
-		destination[i] = source[i];
-		return (destination + i + 1);
 	}
 	return (NULL);
 }
