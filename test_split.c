93240a9008cb44f30391e48cea9753e3ba8d34ef
Dorine Powdered <dpowdere@student.21-school.ru>
2020-12-24 21:48:52 +0300

    Extend split test with preprocessor commands to count for both ft_split and ft_split_const

--- test_split.c
+++ test_split.c
@@ -1,23 +1,24 @@
 #include <unistd.h>
 #include <stdlib.h>
+
 #include "libft.h"
+
+#ifdef SPLIT_CONST
+# define ft_split	ft_split_const
+#endif
 
 #define ft_print(s) ft_putstr_fd(s, 1)
 #define ft_println(s) ft_putendl_fd(s, 1)
 
-/*
-ft_print("Words: ");
-ft_putnbr_fd(n_words, 1);
-ft_print(", chars: ");
-ft_putnbr_fd(n_chars, 1);
-ft_print("\n");
-*/
-
-void ft_wmtest(char **tabstr, char *test, char *s1, char c)
+void ft_test(char *test, char *s1, char c)
 {
-	int		i;
+	int			i;
+#	ifdef SPLIT_CONST
+	char *const	*tabstr;
+#	else
+	char		**tabstr;
+#	endif
 
-	i = 0;
 	tabstr = NULL;
 	ft_print("\n\x1b[33m");
 	ft_print(test);
@@ -27,74 +28,50 @@ void ft_wmtest(char **tabstr, char *test, char *s1, char c)
 	ft_print("\x1b[0m], test char: [\x1b[31m");
 	ft_putchar_fd(c, 1);
 	ft_print("\x1b[0m]\nTest result: [\n");
-		if (!(tabstr = ft_split(s1, c)))
-			ft_println("NULL");
-		else
+	tabstr = ft_split(s1, c);
+	if (!tabstr)
+		ft_println("NULL");
+	else
+	{
+		i = 0;
+		while (tabstr[i] != NULL)
 		{
-			while (tabstr[i] != NULL)
-			{
-				ft_print("- ");
-				ft_print(tabstr[i]);
-				write(1, "\n", 1);
-				i++;
-			}
+			ft_print("- ");
+			ft_print(tabstr[i]);
+#			ifndef SPLIT_CONST
+			free(tabstr[i]);
+#			endif
+			write(1, "\n", 1);
+			i++;
 		}
+	}
 	ft_print("]\n");
+	free((void *)tabstr);
 }
 
 int main()
 {
-	char **t;
-
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 1",
+	ft_test("War machine, ft_split, test 1",
 		"          ",
 		' ');
-	free(t);
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 2",
+	ft_test("War machine, ft_split, test 2",
 		"lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse",
 		' ');
-	free(t);
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 3",
+	ft_test("War machine, ft_split, test 3",
 		"   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ",
 		' ');
-	free(t);
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 4",
+	ft_test("War machine, ft_split, test 4",
 		"lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
 		'i');
-	free(t);
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 5",
+	ft_test("War machine, ft_split, test 5",
 		"lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
 		'z');
-	free(t);
-	t = NULL;
-	ft_wmtest(t, "War machine, ft_split, test 6", "", 'z');
-	free(t);
-	t = NULL;
-
-	ft_wmtest(t, "ft_split, 7", "qwer\0asdf", 'm');
-	free(t);
-	t = NULL;
-
-	ft_wmtest(t, "ft_split, 8", "qwer\0asdf", '\0');
-	free(t);
-	t = NULL;
-
-	ft_wmtest(t, "ft_split, 9", "", '\0');
-	free(t);
-	t = NULL;
-
-	ft_wmtest(t, "ft_split, 10", "Hello, !", '\0');
-	free(t);
-	t = NULL;
-
-	ft_wmtest(t, "ft_split, 10", "Hello, beautiful world!", '\0');
-	free(t);
-	t = NULL;
+	ft_test("War machine, ft_split, test 6", "", 'z');
+	ft_test("ft_split, 7", "qwer\0asdf", 'm');
+	ft_test("ft_split, 8", "qwer\0asdf", '\0');
+	ft_test("ft_split, 9", "", '\0');
+	ft_test("ft_split, 10", "Hello, !", '\0');
+	ft_test("ft_split, 11", "Hello, beautiful world!", '\0');
 
 	return (0);
 }
