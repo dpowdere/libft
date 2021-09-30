2821bf2157843be0b6ba17222ad7033c116387b7
Dorine Powdered <dpowdere@student.21-school.ru>
2021-09-30 22:31:17 +0300

    Make the return value of `ft_write` more like that of the `write` syscall

--- ft_write.c
+++ ft_write.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 20:42:42 by dpowdere          #+#    #+#             */
-/*   Updated: 2021/01/02 20:52:47 by dpowdere         ###   ########.fr       */
+/*   Updated: 2021/09/30 22:29:47 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,26 @@
 
 /*
 ** An iterative equivalent of the `write` system call, returning
-** the value of the last call, not the total bytes written. So it cannot
-** be used when you need to know the exact number of bytes written.
+** the value of the last call, if an error occurs, and the total
+** number of bytes written if every call to `write` is successful.
 */
 
 ssize_t	ft_write(int fd, const void *buf, size_t size)
 {
+	ssize_t	total_size;
 	ssize_t	partial_size;
 
+	total_size = 0;
 	partial_size = write(fd, buf, size);
 	while (partial_size >= 0 && (size_t)partial_size < size)
 	{
+		total_size += partial_size;
 		buf = (unsigned char *)buf + partial_size;
 		size -= partial_size;
 		partial_size = write(fd, buf, size);
 	}
-	return (partial_size);
+	if (partial_size < 0)
+		return (partial_size);
+	total_size += partial_size;
+	return (total_size);
 }
